9899a4ca386ddfc52ba15e8498a09eae42c98736
rushilsrinivas <76115926+rushilsrinivas@users.noreply.github.com>
2021-04-08 12:57:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+menu_list_array = ["Corn Pizza","Pasta Pizza","Cheese magerita","Onion Pizza","Capsicum Pizza","Chilli Pizza","Panner Pizza","Vegetable Pizza","Veg Supreme Pizza"]
+function getmenu(){
+    var htmldata;
+    htmldata="<ol class='menulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++){
+       htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+    }
+    htmldata=htmldata+"</ol>"
+    document.getElementById("display_menu").innerHTML=htmldata;
+}
+function add_item(){
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<div class="cards">' +'<img src="images/pizzaImg.png"/>'+menu_list_array[i]
+
+    }
+    htmldata=htmldata+"</section>"
+    document.getElementById("display_addedmenu").innerHTML=htmldata;
+}
